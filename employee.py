e3212b3523132f2a001c73d24d728e8409835841
sydneyci11 <chun-i.chien@kcl.ac.uk>
2022-12-08 23:55:43 +0000

    test

--- employee.py
+++ employee.py
@@ -66,17 +66,22 @@ class Employee:
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie', pay=4000)
 
+
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie', pay=25, hours=100)
+
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee', pay=3000, commission=200, number=4)
 
+
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan', pay=25, hours=150, number=3, commission=220)
 
+
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie', pay=2000, commission=1500)
+
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel', pay=30, hours=120, commission=600)
